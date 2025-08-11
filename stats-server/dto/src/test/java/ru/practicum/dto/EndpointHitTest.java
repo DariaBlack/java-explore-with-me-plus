aa1b281f7e3f5a57e99abb3279d1c61c2eb4b569
Daria <DariaProtos@yandex.ru>
2025-08-12 00:08:03 +0300

    Редактировать в соответствии с правилами форматирования

--- stats-server/dto/src/test/java/ru/practicum/dto/EndpointHitTest.java
+++ stats-server/dto/src/test/java/ru/practicum/dto/EndpointHitTest.java
@@ -129,14 +129,12 @@ class EndpointHitTest {
 
     @Test
     void testJsonDeserialization() throws Exception {
-        String json = """
-                {
-                  "app": "ewm-main-service",
-                  "uri": "/events/1",
-                  "ip": "192.163.0.1",
-                  "timestamp": "2022-09-06 11:00:23"
-                }
-                """;
+        String json = "{"
+                + "\"app\": \"ewm-main-service\","
+                + "\"uri\": \"/events/1\","
+                + "\"ip\": \"192.163.0.1\","
+                + "\"timestamp\": \"2022-09-06 11:00:23\""
+                + "}";
 
         EndpointHit hit = objectMapper.readValue(json, EndpointHit.class);
 
